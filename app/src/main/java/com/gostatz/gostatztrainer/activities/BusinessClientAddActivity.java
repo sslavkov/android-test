184f7f387c79f9fd939b8977755bf396a06868a2
sslavkov <svet.slavkov@amstatz.com>
2017-08-14 10:00:19 -0500

    save button - todo - repo/dagger

--- app/src/main/java/com/gostatz/gostatztrainer/activities/BusinessClientAddActivity.java
+++ app/src/main/java/com/gostatz/gostatztrainer/activities/BusinessClientAddActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.gostatz.gostatztrainer.R;
 
@@ -23,6 +26,30 @@ public class BusinessClientAddActivity extends AppCompatActivity {
 		
 		setUpActionBar();
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_business_client_add, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_save) {
+			Toast.makeText(this, "Saving...", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
 	
 	private void setUpActionBar() {
 		setSupportActionBar(toolbar);
