6dc4f3e649ceead4ab0a93d2c42efff7e762a2ba
sslavkov <svet.slavkov@amstatz.com>
2017-08-17 22:41:13 -0500

    added keyboard done action event handler clearing errors before validation

--- app/src/main/java/com/gostatz/gostatztrainer/activities/BusinessClientAddFragment.java
+++ app/src/main/java/com/gostatz/gostatztrainer/activities/BusinessClientAddFragment.java
@@ -4,22 +4,24 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Toast;
 
 import com.gostatz.gostatztrainer.R;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.Email;
-import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Length;
 
 import java.util.List;
-import java.util.Map;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 
 /**
@@ -28,29 +30,27 @@ import butterknife.ButterKnife;
 public class BusinessClientAddFragment extends Fragment implements Validator.ValidationListener {
 
 	private Validator validator;
-	private Map<Integer, Integer> editTextToLayoutMap;
 
-	@NotEmpty(messageResId = R.string.error_required)
 	@BindView(R.id.business_client_first_name)
 	TextInputEditText firstName;
+	@Length(trim = true, min = ValidationConstants.NAME_MIN, max = ValidationConstants.NAME_MAX, messageResId = R.string.error_name)
 	@BindView(R.id.layout_business_client_first_name)
 	TextInputLayout firstNameLayout;
 
-	@NotEmpty(messageResId = R.string.error_required)
 	@BindView(R.id.business_client_last_name)
 	TextInputEditText lastName;
+	@Length(trim = true, min = ValidationConstants.NAME_MIN, max = ValidationConstants.NAME_MAX, messageResId = R.string.error_name)
 	@BindView(R.id.layout_business_client_last_name)
 	TextInputLayout lastNameLayout;
 
-//	@NotEmpty
-//	@Email
 	@BindView(R.id.business_client_email)
 	TextInputEditText email;
-
-	@NotEmpty(messageResId = R.string.error_required)
 	@Email(messageResId = R.string.error_email)
 	@BindView(R.id.layout_business_client_email)
 	TextInputLayout emailLayout;
+	
+	@BindViews({ R.id.layout_business_client_first_name, R.id.layout_business_client_last_name, R.id.layout_business_client_email })
+	List<TextInputLayout> allFields; // easy access to all validated fields
 
 	public BusinessClientAddFragment() {
 	}
@@ -62,23 +62,44 @@ public class BusinessClientAddFragment extends Fragment implements Validator.Val
 		ButterKnife.bind(this, rootView);
 
 		setHasOptionsMenu(true); // enables to see menu items
-
-		setUpValidator();
+		initValidator();
+		initEventListeners();
+		
 		return rootView;
 	}
-
-	private void setUpValidator() {
+	
+	private void initEventListeners() {
+		// deals with when keyboard presses done
+		email.setOnEditorActionListener((view, actionId, event) -> {
+			if ((actionId == EditorInfo.IME_ACTION_DONE) || (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER))) {
+				validate();
+			}
+			return false;
+		});
+	}
+	
+	private void initValidator() {
 		validator = new Validator(this);
 		validator.registerAdapter(TextInputLayout.class, new TextInputLayoutAdapter());
 		validator.setValidationListener(this);
 	}
-
+	
+	private void validate() {
+		clearErrors();
+		validator.validate();
+	}
+	
+	private void clearErrors() {
+		for (TextInputLayout field : allFields) {
+			field.setError(null);
+		}
+	}
 
 	@Override
 	public void onValidationSucceeded() {
 		Toast.makeText(getActivity(), "Successful validation", Toast.LENGTH_SHORT).show();
 
-//		NavUtils.navigateUpFromSameTask(getActivity()); // navigate UP
+//		NavUtils.navigateUpFromSameTask(getActivity()); // navigate UP - or go elsewhere
 	}
 
 	@Override
@@ -94,7 +115,6 @@ public class BusinessClientAddFragment extends Fragment implements Validator.Val
 				Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
 			}
 		}
-		Toast.makeText(getActivity(), "Failed validation", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
@@ -111,10 +131,5 @@ public class BusinessClientAddFragment extends Fragment implements Validator.Val
 		}
 
 		return super.onOptionsItemSelected(item);
-	}
-
-	// TODO - check the action from keyboard - to validate
-	private void validate() {
-		validator.validate();
 	}
 }
---
+++ app/src/main/java/com/gostatz/gostatztrainer/activities/ValidationConstants.java
@@ -0,0 +1,12 @@
+package com.gostatz.gostatztrainer.activities;
+
+/**
+ * Created by sslavkov on 8/17/17.
+ */
+
+public class ValidationConstants {
+	
+	public static final int NAME_MIN = 2;
+	public static final int NAME_MAX = 32;
+	
+}
